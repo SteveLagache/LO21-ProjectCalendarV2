4bc34d2f49d27831b83de88da6f32be8906dadac
FGuyet <florian.guyet@etu.utc.fr>
2015-06-06 02:01:33 +0200

    Interface MainWindow

--- Code source/build-ProjectCalendar-Desktop_Qt_5_4_1_MinGW_32bit-Debug/ui_projectcalendar.h
+++ Code source/build-ProjectCalendar-Desktop_Qt_5_4_1_MinGW_32bit-Debug/ui_projectcalendar.h
@@ -13,13 +13,16 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTreeWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -31,7 +34,13 @@ public:
     QAction *actionQuitter_2;
     QWidget *centralwidget;
     QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
     QTreeWidget *treeWidget;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_3;
     QMenuBar *menubar;
     QMenu *menuFichier;
     QMenu *menuA_propos;
@@ -50,11 +59,41 @@ public:
         centralwidget->setObjectName(QStringLiteral("centralwidget"));
         horizontalLayout = new QHBoxLayout(centralwidget);
         horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
         treeWidget = new QTreeWidget(centralwidget);
         treeWidget->setObjectName(QStringLiteral("treeWidget"));
         treeWidget->setMaximumSize(QSize(500, 16777215));
 
-        horizontalLayout->addWidget(treeWidget);
+        verticalLayout->addWidget(treeWidget);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        gridLayout->addWidget(pushButton, 3, 0, 1, 1);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        gridLayout->addWidget(pushButton_2, 2, 0, 1, 1);
+
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
+
+        gridLayout->addWidget(pushButton_4, 2, 1, 1, 1);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        gridLayout->addWidget(pushButton_3, 3, 1, 1, 1);
+
+
+        verticalLayout->addLayout(gridLayout);
+
+
+        horizontalLayout->addLayout(verticalLayout);
 
         ProjectCalendar->setCentralWidget(centralwidget);
         menubar = new QMenuBar(ProjectCalendar);
@@ -85,7 +124,11 @@ public:
         actionQuitter->setText(QApplication::translate("ProjectCalendar", "Charger sauvegarde", 0));
         actionQuitter_2->setText(QApplication::translate("ProjectCalendar", "Quitter", 0));
         QTreeWidgetItem *___qtreewidgetitem = treeWidget->headerItem();
-        ___qtreewidgetitem->setText(0, QApplication::translate("ProjectCalendar", "Titre", 0));
+        ___qtreewidgetitem->setText(0, QApplication::translate("ProjectCalendar", "Titres des t\303\242ches", 0));
+        pushButton->setText(QApplication::translate("ProjectCalendar", "PushButton", 0));
+        pushButton_2->setText(QApplication::translate("ProjectCalendar", "PushButton", 0));
+        pushButton_4->setText(QApplication::translate("ProjectCalendar", "PushButton", 0));
+        pushButton_3->setText(QApplication::translate("ProjectCalendar", "PushButton", 0));
         menuFichier->setTitle(QApplication::translate("ProjectCalendar", "Fichier", 0));
         menuA_propos->setTitle(QApplication::translate("ProjectCalendar", "A propos", 0));
     } // retranslateUi
