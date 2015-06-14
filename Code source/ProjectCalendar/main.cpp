e5e65488e407e82087f7595d2732a33fd28235b4
FGuyet <florian.guyet@etu.utc.fr>
2015-06-14 04:52:18 +0200

    UML + main de test

--- Code source/ProjectCalendar/main.cpp
+++ Code source/ProjectCalendar/main.cpp
@@ -23,6 +23,9 @@ int main(int argc, char* argv[]) {
     TacheUnitaire* t3= tm.ajouterTacheUnitaire("Tâche 3", QDate(2015,1,1), QDate(2016,1,1), Duree(2,0), false);
     TacheUnitaire* t4= tm.ajouterTacheUnitaire("Tâche 4", QDate(2015,1,1), QDate(2016,1,1), Duree(1,0), false);
     TacheUnitaire* t5= tm.ajouterTacheUnitaire("Tâche 7", QDate(2015,1,1), QDate(2016,1,1), Duree(1,0), false);
+    TacheUnitaire* t6= tm.ajouterTacheUnitaire("Tâche 8", QDate(2015,1,1), QDate(2016,1,1), Duree(1,0), true);
+    TacheUnitaire* t7= tm.ajouterTacheUnitaire("Tâche 9", QDate(2015,1,1), QDate(2016,1,1), Duree(1,0), true);
+    TacheUnitaire* t8= tm.ajouterTacheUnitaire("Tâche 10", QDate(2015,1,1), QDate(2016,1,1), Duree(1,0), false);
     TacheComposite* tc1= tm.ajouterTacheComposite("Tâche 5", QDate(2015,1,1), QDate(2016,1,1));
     TacheComposite* tc2= tm.ajouterTacheComposite("Tâche 6", QDate(2015,1,1), QDate(2016,1,1));
 
@@ -32,7 +35,11 @@ int main(int argc, char* argv[]) {
     tc1->ajouterSousTache(tc2);
     tc2->ajouterSousTache(t3);
     tc2->ajouterSousTache(t5);
+
     p2->ajouterTache(t4);
+    p2->ajouterTache(t6);
+    p2->ajouterTache(t7);
+    p2->ajouterTache(t8);
 
     Agenda& a = Agenda::getInstance();
     a.ajouterEvenementSimple("e1","",QDateTime(QDate::currentDate(),QTime(2,0)),QDateTime(QDate::currentDate().addDays(3),QTime(2,0)),"ok");
